78e6d8ef703d36ce9467d3099330eaadfa4876b8
Neha <neharadialcode@gmail.com>
2024-01-29 15:20:21 +0530

    token added

--- src/modules/user.model.js
+++ src/modules/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypy from "bcrypy";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -44,5 +46,34 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
-
+// MIDDLEWARE
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypy.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  bcrypy.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = async function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = async function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 export const User = mongoose.model("User", userSchema);
